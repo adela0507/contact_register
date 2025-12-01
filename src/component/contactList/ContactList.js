19908ef8b55b457e9c44a898f7dff570f24c6366
adela miclea <adelamiclea709@yahoo.com>
2025-12-01 11:24:14 +0200

    contact list

--- src/component/contactList/ContactList.js
+++ src/component/contactList/ContactList.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ContactList(){
+function ContactList({contact}){
+    const[filterText,setFilterText]=useState("");
+    const filterData=contact.filter((item)=>{
+        return Object.keys(item).some((key)=>item[key]
+        .toString().toLowercase().includes(filterText.toLowerCase()))
+    })
     return(
-        <div></div>
+        <div>
+            <h2>Contact List</h2>
+            <input type="text" name="" id="" placeholder="search" onChange={e=>setFilterText(e.target.value)}  />
+            <ul id="list">
+                {filterData.map((item,key)=>
+                <li key={key}>
+                    <span>
+                        {item.full_name}
+                    </span>
+                    <span>
+                        {item.phone_number}
+                    </span>
+                </li>
+                )}
+            </ul>
+                    <span id="totalCount">
+                        Total Contact {filterData.length}
+                    </span>
+
+        </div>
     )
 }
 
